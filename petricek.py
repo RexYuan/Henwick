0a18bf806823a32636f15b6a2fd89ce7ca3d07f7
袁至誠 <r1218r1218@gmail.com>
2017-03-29 14:07:26 +0800

    optimal solution

--- petricek.py
+++ petricek.py
@@ -1,4 +1,18 @@
-# TODO: either think of a way to modify substitute or dynamically generate variables
+# 1) Non terminal z3 sort
+# 2) z3 variable?
+# 3) commutative for all functions? => for greedy expansion
+# 4) cant check semantic equivalence? (a and b) != (b and a) => for historic prune
+# 5) chatbot => cf botman
+#    user design by DFA and friendly syntax. synthesized a corresponding syntax for it
+#    - simply macro?
+#    - what is and how to check correctness? can SMT apply?
+#    - merely state and transition code? what if theres NL...
+#    - is this more like a compiler
+# 6) whats a "good" grammar? (I = I + I | T) vs (I = I + T)
+#    and does this affect my algo?
+#    excluding not and > >=, redundancy?
+# 7) what can invariant insight do? => dynamically changing grammar order?
+# 8) is_implied? can i use Or(Not(a), b)
 
 """
 Synthesize a loop invariant expression for Tomas Petricek's
@@ -15,9 +29,9 @@ example program retrieved from http://stackoverflow.com/a/3221583/2448540
 # }
 
 # Goal:
-# Given program pre-condition = (| T |)
+# Given program, program pre-condition = (| T |)
 # and post-condition = (| i = 10 and j = -1 |),
-# Find invariant = (| i + j == 9 and i < 11 |).
+# find the invariant = (| i + j == 9 and i < 11 |).
 
 # Proof:
 # (| T |)
@@ -39,35 +53,36 @@ example program retrieved from http://stackoverflow.com/a/3221583/2448540
 # (| i = 10 and j = -1 |) Implied
 
 # Requirements:
-# Given an eta,
+# Given some eta, check its correctness by try to
 # prove the validity of these three implications:
 # 1) T => eta[j/9][i/0]
 # 2) eta and not i < 10 => i = 10 and j = -1
 # 3) i < 10 and eta => eta[j-1/j][i+1/i]
 
-# Strategy:
+# Strategies:
 # 1) Prune with logical requirements for invariant.
-# Given an expression E for eta, for any failure in checking some requirements R,
+#   Given an expression E for eta, for any failure in checking some requirements R,
 # SMT solver Z3 would return a particular model M (valuation), that makes R, with
 # eta substituted with E, evaluate to false, when the variables in eta are
 # substituted with the corresponding concrete values in M.
-# Thus, we can examine the form of R and devise pruning strategies from the fact
+#   Thus, we can examine the form of R and devise pruning strategies from the fact
 # that M resolves much of R but the part of eta, where it can only be checked
 # after substituting with some expression E. So, with the knowledge of the form
 # of R and how everything but eta in R evaluates to under M, we need only to check
 # what E evaluates to under M to determine if R holds or not.
-# For requirement 1), T => eta[j/9][i/0], since eta is in the concequent slot of
+#   For requirement 1), T => eta[j/9][i/0], since eta is in the concequent slot of
 # an implication, and under any valuation T evaluates to True, if eta[j/9][i/0]
 # evaluates to False under M, this should be pruned.
-# For requirement 2), eta and not i < 10 => i = 10 and j = -1, if we know from Z3
+#   For requirement 2), eta and not i < 10 => i = 10 and j = -1, if we know from Z3
 # that, for an expression E1 and model M, this requirement is False, we know that
 # , under M, E1 and not i < 10 evaluates to True and i = 10 and j = -1 to False.
-# So, for an expression E2, we need only to check whether E2 evaluates to True
+#   So, for an expression E2, we need only to check whether E2 evaluates to True
 # under M to decide if this should be pruned, and thus saving time asking Z3.
-# For i < 10 and eta => eta[j-1/j][i+1/i], if i < 10 and E1 => E1[j-1/j][i+1/i]
+#   For i < 10 and eta => eta[j-1/j][i+1/i], if i < 10 and E1 => E1[j-1/j][i+1/i]
 # evaluates to False under M, E1 must evaluates to True and E1[j-1/j][i+1/i] to
 # False, respectively, under M. So, we only need to check if E2 is True and
 # E2[j-1/j][i+1/i] is False to prune it off.
+#
 # 2) NOTE: how to Prune with semantic requirements.
 # simplify to true or false
 # 3) NOTE: Prune with history or semantic equivalence like commutativity
@@ -76,7 +91,7 @@ example program retrieved from http://stackoverflow.com/a/3221583/2448540
 
 # grammar:
 # S    ::= Bool
-# Bool ::= And(Bool, Bool) | Or(Bool, Bool) | Implies(Bool, Bool) |
+# Bool ::= And(Bool, Bool) | Or(Bool, Bool) |
 #          Int > Int | Int >= Int | Int == Int
 # Int  ::= Term | Term + Term | Term - Term
 # Term ::= Cst | Var
@@ -88,7 +103,7 @@ from time import time
 
 class Z3:
     def __init__(self):
-        NT = DeclareSort('Nonterminal')
+        #NT = DeclareSort('Nonterminal')
         self._S     = Bool('S')
         self._Bool  = Bool('Bool')
         self._Int   = Int('Int')
@@ -100,33 +115,38 @@ class Z3:
 
         self.prod = {
             self._S:    [self._Bool],
-            self._Bool: [Or(self._Bool, self._Bool),
+            self._Bool: [And(self._Bool, self._Bool),
+                         Or(self._Bool, self._Bool),
                          self._Int > self._Int,
                          self._Int >= self._Int,
-                         self._Int == self._Int,
-                         And(self._Bool, self._Bool)],
+                         self._Int == self._Int],
             self._Int:  [self._Term,
-                         self._Term - self._Term,
-                         self._Term + self._Term],
+                         self._Term + self._Term,
+                         self._Term - self._Term],
             self._Term: [self._Cst,
                          self._Var],
-            self._Cst:  [IntVal(11),
-                         IntVal(9)],
+            self._Cst:  [IntVal(9),
+                         IntVal(11)],
             self._Var:  [self.i,
                          self.j]
         }
+
         self.solver = Solver()
+
         self.counter_examples1 = []
         self.counter_examples2 = []
         self.counter_examples3 = []
         self.history = []
+
         self.query_counter = 0
+        self.prunes_counter = 0
         self.pruneh_counter = 0
         self.prune1_counter = 0
         self.prune2_counter = 0
         self.prune3_counter = 0
 
     def isImpValid(self, imp):
+        """check the validity of imp"""
         self.solver.push()
         self.solver.add(Not(imp))
         result = self.solver.check()
@@ -135,6 +155,8 @@ class Z3:
         return ret
 
     def checkEta(self, eta):
+        """check eta against requirements"""
+        self.query_counter+=1
         # req 1) T => eta[j/9][i/0]
         result = self.isImpValid(Implies(True, substitute(eta, (self.j, IntVal(9)), (self.i, IntVal(0)))))
         if result != True:
@@ -156,18 +178,21 @@ class Z3:
         return True
 
     def pruned(self, exp):
-        # NOTE strat 2)
-        """if simplify(exp) == True or simplify(exp) == False:
+        """check if exp is pruned"""
+        # semantic pruning
+        if (not any(c in self.prod for c in self.getConstituents(exp)) and
+            (simplify(exp) == True or simplify(exp) == False)):
+            self.prunes_counter += 1
             return True
-        if simplify(exp) in self.history:
-            return True
-        self.history.append(simplify(exp))
-        """
-        # pruning with counter examples
+
+        # historic pruning
+        #if simplify(exp) in self.history:
+        #    self.pruneh_counter += 1
+        #    return True
+        #self.history.append(simplify(exp))
+
+        # counter example pruning
         # T => eta[j/9][i/0]
-        #if any(simplify(substitute(substitute(exp, (self.j, IntVal(9)), (self.i, IntVal(0))),
-        #       (self.i, ci), (self.j, cj))) == False
-        #       for (ci, cj) in self.counter_examples1):"""
         if simplify(substitute(exp, (self.j, IntVal(9)), (self.i, IntVal(0)))) == False:
             self.prune1_counter += 1
             return True
@@ -177,9 +202,6 @@ class Z3:
             self.prune2_counter += 1
             return True
         # i < 10 and eta => eta[j-1/j][i+1/i]
-        #if any(simplify(substitute(exp, (self.i, ci), (self.j, cj))) == True
-        #       and simplify(substitute(substitute(exp, (self.j, self.j-IntVal(1)), (self.i, self.i+IntVal(1))),
-        #       (self.i, ci), (self.j, cj))) for (ci, cj) in self.counter_examples3):
         if any(simplify(substitute(exp, (self.i, ci), (self.j, cj))) == True
                and simplify(substitute(exp, (self.j, cj-IntVal(1)),
                                             (self.i, ci+IntVal(1)))) == False
@@ -188,6 +210,14 @@ class Z3:
             return True
 
     def getConstituents(self, exp):
+        """
+        Generator for single terms(offsprings) in exp, ordered by DFS
+
+        >> x, y = Bools('x y')
+        >> a, b = Ints('a b')
+        >> list(getConstituents(And(a > b, Or(x, Implies(a <= b, y)))))
+        >> [a, b, x, a, b, y]
+        """
         for c in exp.children():
             if c.children():
                 yield from self.getConstituents(c)
@@ -195,45 +225,27 @@ class Z3:
                 yield c
 
     def genesis(self, exp, limit):
-        print(exp)
-        #if simplify(exp) == True or simplify(exp) == False:
-        #    print("WW")
-        #    return False
-        """if simplify(exp) in self.history:
-            return False
-        self.history.append(simplify(exp))
-        """
+        """DLS on offspring count bounded by limit"""
         offsprings = list(self.getConstituents(exp))
         children = exp.children()
         # depth limited search
         if (len(offsprings) > limit or
            (len(offsprings) == limit and any(c == self._Bool for c in offsprings)) or
            sum(2 if c == self._Bool else 1 for c in offsprings) > limit):
-            #print("p")
             return False
-        # recursion base
-        # if expression has children and they're all termini or
-        # if expression has no child and is a terminus
+        # recursion base: if expression has children and they're all termini or
+        #                    expression has no child and is a terminus
         if (children and not any(c in self.prod for c in offsprings) or
             not children and exp not in self.prod):
-            #print("H")
             # pruning
             if self.pruned(exp):
                 return False
             # query SMT solver
-            self.query_counter+=1
             if self.checkEta(exp):
                 return exp
             else:
                 return False
         # expression expansion
-        # NOTE: search strat? how to find insight for goal from other things?
-        #       such as post like said from textbook? or anyway get some general
-        #       progenitive direction from context? like changning grammar on the fly
-        #       or pruning off symmetric function application such as "And, Or"
-        #       when args are semantically equivalent in the sense that their
-        #       production rules are the same?
-        #       I should go back and read more about classic AI search for insight
         # single term
         if not offsprings:
             for p in self.prod[exp]:
@@ -245,51 +257,44 @@ class Z3:
         else:
             for i, c in enumerate([c for c in offsprings]):
                 if c in self.prod:
-                    #print(self.prod[c])
                     for p in self.prod[c]:
-                        #print(exp, c, p, i)
                         ret = self.genesis(self.sub(exp, c, p, i), limit)
                         # eta is found
                         if type(ret) == BoolRef:
                             return ret
+                    # greedy
                     return False
+        # eta not under this expansion
         return False
 
     def synthesis(self, limit):
+        """IDS on offspring count bounded by limit"""
+        self.synth_time = time()
+        self.lim = limit
+        self.inv = None
         # iterative deepening search
-        for l in range(5, limit+1):
+        for l in range(1, limit+1):
             result = self.genesis(self._S, l)
-            #print("==========================")
-            #print("level",l)
-            #self.report()
-            #print("==========================")
             # eta is found
             if type(result) == BoolRef:
+                self.synth_time = time() - self.synth_time
+                self.inv = result
                 return result
 
     def test(self):
-        #print(self.checkEta(self.i+self.j==IntVal(9)))
-        #print(self.checkEta(And(self.i+self.j==IntVal(9), self.i < IntVal(11))))
-
-        self.w,self.x,self.y,self.z,self.m = Bools('w x y z m')
-        #self.a,self.b,self.c,self.d,self.n = Ints('a b c d n')
-
-        # From And(w, Or(x, b<d, a-b<=c, x, x), a+b==c-a, x)
-        # To   And(w, Or(x, b<d, n-b<=c, x, x), a+b==c-a, x)
-        #print(And(self.x, self.x))
-        #print(self.sub(And(self.x, self.x), self.x, self.y))
-        print(self.sub(And(self.x, self.y, self.x, self.x), self.x, self.y, 1))
-        #print(And(self.w, Or(self.x, self.b<self.d, self.a-self.b<=self.c, self.x, self.x), self.a+self.b==self.c-self.a, self.x))
-        #print(self.sub(And(self.w, Or(self.x, self.b<self.d, self.a-self.b<=self.c, self.x, self.x), self.a+self.b==self.c-self.a, self.x), self.a, self.n))
-        # From And(w, Or(x, b<d, a-b<=c, x, x), a+b==c-a, x)
-        # To   And(w, Or(m, b<d, a-b<=c, x, x), a+b==c-a, x)
-        #print(And(w, Or(x, b<d, a-b<=c, x, x), a+b==c-a, x))
-        #print(self.sub(And(w, Or(x, b<d, a-b<=c, x, x), a+b==c-a, x), x, m))
+        print(self.checkEta(self.i+self.j==IntVal(9)))
+        print(self.checkEta(And(self.i+self.j==IntVal(9), self.i < IntVal(11))))
         return
 
     def report(self):
-        # stats
+        """synthesis stats report"""
+        print("==========================")
+        if type(self.inv) == BoolRef:
+            print("invariant found:", self.inv)
+        else:
+            print("invariant not found within depth:", self.lim)
         print("z3 queried:", z3.query_counter, "\n")
+        print("semantics pruned:", z3.prunes_counter, "\n")
         print("history size:", len(self.history))
         print("history pruned:", z3.pruneh_counter, "\n")
         print("ce 1 size:", len(self.counter_examples1))
@@ -298,9 +303,12 @@ class Z3:
         print("req 2 pruned:", z3.prune2_counter, "\n")
         print("ce 3 size:", len(self.counter_examples3))
         print("req 3 pruned:", z3.prune3_counter, "\n")
+        print("time:", self.synth_time)
+        print("==========================")
         return
 
     def subv(self, exp, oldv, newv, order):
+        """sub recursion helper"""
         parts = exp.children()
         if is_const(exp):
             if exp == oldv and not self.done and self.subc == order:
@@ -313,35 +321,64 @@ class Z3:
             return And([self.subv(parts[i], oldv, newv, order) for i in range(len(parts))])
         elif is_or(exp):
             return Or([self.subv(parts[i], oldv, newv, order) for i in range(len(parts))])
+        elif is_not(exp):
+            return Not(self.subv(parts[0], oldv, newv, order))
         elif is_add(exp):
-            return self.subv(parts[0], oldv, newv, order) + self.subv(parts[1], oldv, newv, order)
+            return self.subv(parts[0], oldv, newv, order) +  self.subv(parts[1], oldv, newv, order)
         elif is_sub(exp):
-            return self.subv(parts[0], oldv, newv, order) - self.subv(parts[1], oldv, newv, order)
+            return self.subv(parts[0], oldv, newv, order) -  self.subv(parts[1], oldv, newv, order)
+        elif is_mul(exp):
+            return self.subv(parts[0], oldv, newv, order) *  self.subv(parts[1], oldv, newv, order)
         elif is_le(exp):
             return self.subv(parts[0], oldv, newv, order) <= self.subv(parts[1], oldv, newv, order)
         elif is_lt(exp):
-            return self.subv(parts[0], oldv, newv, order) < self.subv(parts[1], oldv, newv, order)
+            return self.subv(parts[0], oldv, newv, order) <  self.subv(parts[1], oldv, newv, order)
         elif is_ge(exp):
             return self.subv(parts[0], oldv, newv, order) >= self.subv(parts[1], oldv, newv, order)
         elif is_gt(exp):
-            return self.subv(parts[0], oldv, newv, order) > self.subv(parts[1], oldv, newv, order)
+            return self.subv(parts[0], oldv, newv, order) >  self.subv(parts[1], oldv, newv, order)
         elif is_eq(exp):
             return self.subv(parts[0], oldv, newv, order) == self.subv(parts[1], oldv, newv, order)
         else:
             raise Exception("uncaught sub", exp, oldv, newv, order)
 
-    def sub(self, exp, oldv, newv, order=1):
+    def sub(self, exp, oldv, newv, order=0):
+        """
+        Substitute oldv of index order with newv in exp.
+        oldv should be single term expression.
+        supported symbols: And, Or, Not, +, -, *, <=, <, >=, >, ==
+
+        >> x, y = Bools('x y')
+        >> sub(And(x, x), x, y)
+        And(y, x)
+        >> sub(And(x, x), x, y, 1)
+        And(x, y)
+        >> sub(And(y, x, x, x), x, y, 2)
+        And(y, x, y, x)
+
+        >> sub(And(Or(x, y), x), Or(x, y), y)
+        Exception: sub oldv should be single term expression
+        >> sub(And(x, Or(x, y)), x, Or(x, y))
+        And(Or(x, y), Or(x, y))
+
+        >> sub(And(x, x), y, And(x, y))
+        And(x, x)
+        >> sub(And(x, x), x, y, 5)
+        And(x, x)
+
+        >> sub(Implies(x, x), x, y)
+        Exception: ('uncaught sub', Implies(x, x), x, y, 0)
+        """
+        if not is_const(oldv):
+            raise Exception("sub oldv should be single term expression")
         self.done = False
         self.subc = 0
         return self.subv(exp, oldv, newv, order)
 
 z3 = Z3()
 t = time()
-synthed = z3.synthesis(5)
-print("==========================")
-print("exp =", synthed)
+
+z3.synthesis(10)
 z3.report()
-print("time:", time()-t)
-print("==========================")
 
 #z3.test()
