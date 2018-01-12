9a110fef7181360d501b13f8e05cbc0db10146b2
Rex Yuan <r1218r1218@gmail.com>
2018-01-13 01:42:34 +0800

    check em long boi

--- exp.py
+++ exp.py
@@ -3,12 +3,6 @@ from time import time
 from itertools import permutations
 from random import sample, choice
 
-s = list('abcdexyz')
-ps = list(map(lambda l:''.join(l), list(permutations(s))))
-# 40 hard thingly
-#t = {'abecd', 'dcaeb', 'cedba', 'cabde', 'caebd', 'baced', 'dacbe', 'cebad', 'cdabe', 'badce', 'acebd', 'bcaed', 'acbde', 'acbed', 'aecbd', 'aecdb', 'daecb', 'acdeb', 'cdaeb', 'decab', 'ecdab', 'adceb', 'abced', 'ceadb', 'daceb', 'adecb', 'deabc', 'dbace', 'ecbad', 'caedb', 'acdbe', 'abcde', 'ceabd', 'acedb', 'eacdb', 'bacde', 'cedab', 'edcab', 'deacb', 'bdace'}
-#lins = sample(ps, 110)
-
 lins = []
 tmp = choice(ps)
 last = tmp
