61a88c65bcd4a5fd52d284e40d6dd6537b18ad34
Rex Yuan <r1218r1218@gmail.com>
2018-01-21 21:59:16 +0800

    prep graph and new batch

--- cal.py
+++ cal.py
@@ -1,19 +1,38 @@
-from termcolor import cprint
-from pprint import pprint
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
-table = [[k * n**2 for k in range(1,21)] for n in range(1,21)]
+with open('/Users/Rex/Desktop/result/timeout_log.txt') as fp:
+    s = fp.read()
+    stats = eval('['+s[:-1]+']')
+'''
+lins = list(map(lambda l:l[0], stats))
+omega = list(map(lambda l:l[1], stats))
+k = list(map(lambda l:l[2], stats))
+d = list(map(lambda l:l[3], stats))
+r = list(map(lambda l:l[4], stats))
+'''
+# lins = 50, 60, 80, 100
+# omega = 5, 6, 8 10
+record = {lins: {omega: 0 for omega in [5,6,8,10]} for lins in [50,60,80,100]}
+for lins, omega, k, d, r in stats:
+    record[lins][omega] += 1
 
-for n in range(0,21):
-    s = "{:>4}".format(str(n))
-    print(s, end=' ')
-print()
-for n in range(0,20):
-    s = "{:>4}".format(str(n+1))
-    print(s, end=' ')
-    for k in range(0,20):
-        s = "{:>4}".format(str(table[n][k]))
-        if table[n][k] > 300:
-            cprint(s, 'red', 'on_cyan', end=' ')
-        else:
-            print(s, end=' ')
-    print()
+'''
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+coordinates = np.meshgrid(np.arange(10), np.arange(10))
+
+x = coordinates[0].ravel()
+y = coordinates[1].ravel()
+z = np.zeros(100)
+
+dx = np.ones(100)
+dy = np.ones(100)
+dz = np.arange(100)
+
+ax.bar3d(x, y, z, dx, dy, dz, shade=True)
+
+plt.show()
+'''
