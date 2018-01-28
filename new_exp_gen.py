85ff21bba261ba178db94e2b0ce1449a10074176
Rex Yuan <r1218r1218@gmail.com>
2018-01-28 23:00:30 +0800

    remove 4 ele and 20 lins

--- new_exp_gen.py
+++ new_exp_gen.py
@@ -4,8 +4,8 @@ with open('batch template.py') as fp:
     rest = fp.read()
 
 i = 1
-for ele in [4,5,6,7,8,9,10]:
-    for lin in [20,30,40,50,60,70,80,90,100]:
+for ele in [5,6,7,8,9,10]:
+    for lin in [30,40,50,60,70,80,90,100]:
         config = "batch = {batch};ele_count = {ele_count};lin_count = {lin_count}\n".format(batch=i, ele_count=ele, lin_count=lin)
         with open("new exp batch/batch{}.py".format(i), 'w') as fp:
             fp.write(config+rest)
