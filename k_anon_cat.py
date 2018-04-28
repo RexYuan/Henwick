99a4e84f63291e42480095cb9efaa49c8242070e
Rex Yuan <r1218r1218@gmail.com>
2018-04-28 18:04:54 +0800

    err

--- k_anon_cat.py
+++ k_anon_cat.py
@@ -10,8 +10,8 @@ import pymysql.cursors
 
 from census99_attr import *
 
-log_path = 'log'
-pickle_path = 'ret'
+log_path = '../catlog'
+pickle_path = '../ret.p'
 
 def get_value(fields):
     connection = pymysql.connect(host='localhost',
@@ -37,19 +37,25 @@ def get_row(fvalues):
         clause += " {}={} AND".format(f, v)
     clause = clause[1:-4]
 
-    connection = pymysql.connect(host='localhost',
-                                 user='rex',
-                                 password='rex',
-                                 db='census99',
-                                 cursorclass=pymysql.cursors.DictCursor)
+    while True:
+        try:
+            connection = pymysql.connect(host='localhost',
+                                         user='rex',
+                                         password='rex',
+                                         db='census99',
+                                         cursorclass=pymysql.cursors.DictCursor)
 
-    with connection.cursor() as cursor:
-        sql = ("SELECT COUNT(*) FROM PEOPLE WHERE {}").format(clause)
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        row_count = list(result.values())[0]
+            with connection.cursor() as cursor:
+                sql = ("SELECT COUNT(*) FROM PEOPLE WHERE {}").format(clause)
+                cursor.execute(sql)
+                result = cursor.fetchone()
+                row_count = list(result.values())[0]
 
-    connection.close()
+            connection.close()
+        except:
+            continue
+        break
+    #print(connection.open, flush=True)
 
     logger = logging.getLogger("field[{}]".format(fvalues))
     logger.setLevel(logging.DEBUG)
@@ -64,8 +70,8 @@ def get_row(fvalues):
     status_fh.setFormatter(status_formatter)
     status_sh.setFormatter(status_formatter)
 
-    #logger.addHandler(status_fh)
-    logger.addHandler(status_sh)
+    logger.addHandler(status_fh)
+    #logger.addHandler(status_sh)
 
     logger.info(str(row_count))
 
@@ -74,13 +80,13 @@ def get_row(fvalues):
 if __name__ == '__main__':
     fields = [('C020_AGE', 'C060_REL_HEAD', 'C072_EDUCATION', 'C122_INDUSTRY', 'C123_OCCUPATION')]
 
-    procs = 3
+    procs = 30
 
     for field in fields:
         tt = time()
 
         with Pool(processes=procs) as pool:
-            ret = pool.map(get_row, islice(get_value(field),10))
+            ret = pool.map(get_row, get_value(field),10)
             pickle.dump(ret, open(pickle_path, "wb"))
 
         print("Time: "+str(time()-tt))
